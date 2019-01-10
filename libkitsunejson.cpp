eaab7c5a5270b162ad1064658b9419f9f3c1bea5
Tobias Anker <tobias.anker@kitsunemimi.moe>
2019-01-10 21:37:29 +0100

    initial commit

---
+++ libkitsunejson.cpp
@@ -0,0 +1,6 @@
+#include "libkitsunejson.h"
+
+
+LibKitsuneJson::LibKitsuneJson()
+{
+}
---
+++ libkitsunejson.h
@@ -0,0 +1,12 @@
+#ifndef LIBKITSUNEJSON_H
+#define LIBKITSUNEJSON_H
+
+
+class LibKitsuneJson
+{
+
+public:
+    LibKitsuneJson();
+};
+
+#endif // LIBKITSUNEJSON_H
