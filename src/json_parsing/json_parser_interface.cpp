2f104991d9a744c15e0e4f31dc9e6e1f8a160750
Tobias Anker <tobias.anker@kitsunemimi.moe>
2021-11-22 15:30:32 +0100

    fix crash in some cases of string-parsing

--- src/json_parsing/json_parser_interface.cpp
+++ src/json_parsing/json_parser_interface.cpp
@@ -96,7 +96,8 @@ JsonParserInterface::parse(const std::string &inputString,
     this->scan_end();
 
     // handle negative result
-    if(parserResult != 0)
+    if(parserResult != 0
+            || m_errorMessage.size() > 0)
     {
         error.addMeesage(m_errorMessage);
         LOG_ERROR(error);
@@ -168,6 +169,10 @@ void
 JsonParserInterface::error(const Kitsunemimi::Json::location& location,
                            const std::string& message)
 {
+    if(m_errorMessage.size() > 0) {
+        return;
+    }
+
     // get the broken part of the parsed string
     const uint32_t errorStart = location.begin.column;
     const uint32_t errorLength = location.end.column - location.begin.column;
@@ -176,16 +181,22 @@ JsonParserInterface::error(const Kitsunemimi::Json::location& location,
     std::vector<std::string> splittedContent;
     splitStringByDelimiter(splittedContent, m_inputString, '\n');
 
-    // -1 because the number starts for user-readability at 1 instead of 0
-    const std::string errorStringPart = splittedContent[linenumber - 1].substr(errorStart - 1,
-                                                                               errorLength);
-
     // build error-message
     m_errorMessage =  "ERROR while parsing json-formated string \n";
     m_errorMessage += "parser-message: " + message + " \n";
     m_errorMessage += "line-number: " + std::to_string(linenumber) + " \n";
-    m_errorMessage += "position in line: " + std::to_string(location.begin.column) + " \n";
-    m_errorMessage += "broken part in string: \"" + errorStringPart + "\" \n";
+
+    if(splittedContent[linenumber - 1].size() > errorStart - 1 + errorLength)
+    {
+        m_errorMessage.append("position in line: " +  std::to_string(location.begin.column) + "\n");
+        m_errorMessage.append("broken part in string: \""
+                              + splittedContent[linenumber - 1].substr(errorStart - 1, errorLength)
+                              + "\"");
+    }
+    else
+    {
+        m_errorMessage.append("position in line: UNKNOWN POSITION (maybe a string was not closed)");
+    }
 }
 
 }  // namespace Json
